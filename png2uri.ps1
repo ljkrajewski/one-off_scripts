7facd8f1dda665cdf4058566d757029df17795b2
ljkrajewski <50240322+ljkrajewski@users.noreply.github.com>
2024-04-25 11:25:11 -0400

    Update png2uri.ps1

--- png2uri.ps1
+++ png2uri.ps1
@@ -1,3 +1,10 @@
+##############################################################
+# png2uri.ps1
+#
+# Powershell script to convert a PNG image to URI. Writes a
+# ".uri" file with just the image data and an HTML page with
+# the image inside it.
+##############################################################
 param(
   [Parameter(Mandatory=$true)]
   [String] $ImagePath
@@ -7,6 +14,7 @@ Write-Host "`nReading PNG file from: $ImagePath"
 
 $bytes = Get-Content -Path $ImagePath -Encoding Byte
 $base64Encoded = [Convert]::ToBase64String($bytes)
+$mimeType = Get-Item $ImagePath -Force | Select-Object ContentType
 
 $uri = "data:image/png;base64,$base64Encoded"
 
@@ -20,3 +28,21 @@ $outputPath = Join-Path (Split-Path -Path $ImagePath) -ChildPath $filename
 Set-Content -Path $outputPath -Value $uri -Encoding UTF8
 
 Write-Host "Data URI written to:  $outputPath"
+
+# Get filename and change extension to .html
+$htmlfilename = (Get-Item $ImagePath).BaseName + ".html"
+
+# Build output path with modified filename
+$htmloutputPath = Join-Path (Split-Path -Path $ImagePath) -ChildPath $htmlfilename 
+
+# Write web page to file
+@"
+<!DOCTYPE html>
+<html lang="en">
+<body>
+  <img src="$uri">
+</body>
+</html>
+"@ | out-file -Encoding UTF8 $htmloutputPath
+
+Write-Host "HTML page written to:  $htmloutputPath"
